101117032de548093b0f625fdc90275514295893
mehedirayhan1122 <mehedirayhan1122@.com>
2024-11-13 00:15:03 +0600

    updated

--- src/test/java/testRunner/TransactionTestRunner.java
+++ src/test/java/testRunner/TransactionTestRunner.java
@@ -13,7 +13,7 @@ import java.io.IOException;
 
 public class TransactionTestRunner extends Setup {
 
-    @Test(priority = 1, description = "Transaction 2000tk From System to agent ")
+    @Test(priority = 1, description = "Transaction 2000tk From System to agent")
 
     public void depositFromSystemToAgent() throws IOException, ParseException {
 
@@ -27,7 +27,7 @@ public class TransactionTestRunner extends Setup {
         String messageexpected ="Deposit successful";
         Assert.assertTrue(messageActual.contains(messageexpected));
     }
-    @Test(priority = 2,description = "deposit 1500tk Agent to Customer 1")
+    @Test(priority = 2,description = "Deposit 1500 tk to a customer from the agent account")
     public void depositAgentToCustomer1() throws IOException, ParseException {
 
         TransactionController transactionController = new TransactionController(prop);
@@ -42,7 +42,7 @@ public class TransactionTestRunner extends Setup {
     }
 
 
-    @Test(priority = 3,description = "deposit 1500tk Agent to Customer 1")
+    @Test(priority = 3,description = " Withdraw 500 tk by the customer to the agent")
     public void withdrawCustomer1ToAgent() throws IOException, ParseException {
 
         TransactionController transactionController = new TransactionController(prop);
@@ -57,7 +57,7 @@ public class TransactionTestRunner extends Setup {
     }
 
 
-    @Test(priority = 4,description = "deposit 1500tk Agent to Customer 1")
+    @Test(priority = 4,description = "Send money 500 tk to another customer")
     public void sendMoneyCustomer1ToCustomer2() throws IOException, ParseException {
 
         TransactionController transactionController = new TransactionController(prop);
@@ -71,7 +71,7 @@ public class TransactionTestRunner extends Setup {
         Assert.assertTrue(messageActual.contains(messageexpected));
     }
 
-    @Test(priority = 5,description = "deposit 1500tk Agent to Customer 1")
+    @Test(priority = 5,description = "Payment 100 tk to any merchant by the recipient customer")
     public void paymentCustomer2ToMarchent() throws IOException, ParseException {
 
         TransactionController transactionController = new TransactionController(prop);
@@ -86,7 +86,7 @@ public class TransactionTestRunner extends Setup {
     }
 
 
-    @Test(priority = 6, description = "Check customer balance")
+    @Test(priority = 6, description = "Check balance of the recipient customer")
     public void checkCustomerBalance() throws IOException, ParseException, InterruptedException {
         TransactionController transactionController = new TransactionController(prop);
         Response res = transactionController.checkCustomer2Balance();
